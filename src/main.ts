e721da7afd71a356d545d2f01d5c4531b77135ed
Robby Fratina <46087844+rfrob30@users.noreply.github.com>
2021-08-16 18:34:42 +0800

    Added env control

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.port || 4000);
 }
 bootstrap();
